9db32bef8045709aa363cd1b9fce4bceea005c63
yassindaddacom-png <yassindaddacom@gmail.com>
2026-01-05 23:00:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 
+server: {
         port: 3000,
         host: '0.0.0.0',
       },
